c616cf9489f88926467327040427301ce4d9025c
Darkeyn <56487935+Darkeyn@users.noreply.github.com>
2021-07-21 20:44:38 +0300

    Add files via upload

---
+++ php/textMain.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+$mysqli = new mysqli("localhost","root","root","bd_site");
+$mysqli->query( "SET NAMES 'utf8'");
+
+$str = $mysqli->query("SELECT * FROM `main` WHERE id=(SELECT COUNT(*) FROM `main`)");
+
+if($result = $str){
+    foreach($result as $row){
+         
+        $zag = $row["zag"];
+        $a = $row["a"];
+        $adress = $row["adress"];
+        $text = $row["text"];
+    }
+}
+
+echo json_encode($text);
+
+?>
---
+++ php/textThird.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+$mysqli = new mysqli("localhost","root","root","bd_site");
+$mysqli->query( "SET NAMES 'utf8'");
+
+$str = $mysqli->query("SELECT * FROM `third` WHERE id=(SELECT COUNT(*) FROM `third`)");
+
+if($result = $str){
+    foreach($result as $row){
+         
+        $zag = $row["zag"];
+        $text = $row["text"];
+    }
+}
+
+echo json_encode($text);
+
+?>
